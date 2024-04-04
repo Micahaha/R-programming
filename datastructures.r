95dc8cfa2ec87735ae643ba36a5a2f1519ee5a12
Micahaha <xicer029@gmail.com>
2024-04-04 15:36:10 -0400

    442024

---
+++ datastructures.r
@@ -0,0 +1,96 @@
+# we will take a look at 5 data structures:
+# vector, matrix, array, data frame, list.
+# as we go from vector to matrix and so on, the 
+# structures get more robust, but they also
+# get more complex. 
+
+# a vector is a 1 dimensional collection.
+# the data value put into a vector must be of the 
+# the same data.
+# a vector may consist of 1 elements or multiple
+# elements
+# it's R's most basic data structure.
+
+# create a vector of numeric data values
+v1 <- c(1, 2, 3, 4, 5)
+v1
+
+# create a vector of character data values
+v2 <- c("a","b","c")
+v2
+
+# create a vector of logical data values
+v3 <- c(T,F,F,T,T)
+v3
+
+# a matrix is a 2 dimensional collection.
+# it consists of rows and columns
+# the data values must be of the same data type.
+# use the matrix function to create a matrix.
+
+# create a matrix of logical data values
+m1 <- matrix(
+    c(T,F,F,F,T,F),
+    nrow = 2
+)
+m1
+
+# create a matrix of character data values
+
+m2 <- matrix(
+    c("a", "b", "c", "d"),
+    nrow = 2
+)
+
+m3 <- matrix(
+    c("a", "b", "c"),
+    nrow = 2
+)
+
+# the array may have 3 or more dimensions.
+# the data values must be of the same data type.
+# use the array functions to create arrays.
+
+# create an array of numeric data values (1 - 24)
+a1 <- array(
+    c(1:24),    # specifies the data values
+    c(4,3,2)    # specifies the dimensions - rows, columns, tables
+)
+a1
+
+# a data frame is a 2 dimensional collection that can
+# have vectors of different data types, but the vectors
+# must all be of the same length.
+
+# create 3 vectors all of the same length
+vnumeric <- c(1, 2, 3)
+vcharacter <- c("a","b","c")
+vlogical <- c(T,F,T)
+
+
+# create a data frame combining the vectors together
+# use the as.data.frame and cbind funtions to create
+# a data frame
+df <- as.data.frame(cbind(vnumeric,vcharacter,vlogical))
+df
+
+# the list is one of he most robust and flexible data
+# structures in R because the data values in them may
+# be of any data type and the data structures in them
+# may be of any length.
+
+# create 3 vectors of different data type and length
+o1 <- c(1,2,3)
+o2 <- c("a","b","c","d","e")
+o3 <- c(T,F, F, T)
+
+# create a list using 3 vectors and list function
+list1 <- list(o1,o2,o3)
+list1
+
+# create a list that includes a list
+list2 <- list(o1, o2, o3, list1)
+list2
+
+# clear variables in the global environment
+rm(list = ls())
---
+++ datatypes.r
@@ -0,0 +1,31 @@
+# there are 3 basic data types in R: numeric, character logical
+
+# the numeric ata type ecompasses integers, doubles, and floats
+# double is the default data type
+# assgn a value of 15 to a variable
+n1 <- 15
+n1
+
+# typeof will tell you the data type of the variable it's given
+typeof(n1)
+
+# assign a value of 1.5 to a variable
+n2 <- 1.5
+n2
+typeof(n1)
+
+# assign a value of 15 as an integer to a variable
+n3 <- as.integer(15)
+n3
+typeof(n3)
+
+# the character data type encompasses single characters as well as
+# strings of characters
+# there is no string type in R
+# assign the single character "c" to a variable
+c1 <- "c"
+c1
+typeof(c1)
+
+# assign a string of characaters to a variable
+c2 <- "a string of characters"
